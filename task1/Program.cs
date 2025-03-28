60ff4d7993a34b312a2674a2589f76587e5a3243
Kaushik <107951957+kaushikswift@users.noreply.github.com>
2025-03-29 02:46:02 +0530

    Added task1:factorial app

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Program  // Renamed 'Main' to 'Program'
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine()); // Convert input to integer
+        int fact = Factorial(num);
+        Console.WriteLine($"Factorial of {num} is: {fact}");
+    }
+
+    static int Factorial(int n)  // Parameter name corrected
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+}
